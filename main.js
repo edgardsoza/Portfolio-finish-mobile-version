62532f3c8ae19d80bfc87016257bbdbc5a3b2fcb
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-02-07 09:36:37 -0600

    fixing linter errors

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ const toggle = document.querySelector('.menuicon');
 const weboptions = document.querySelector('.weboptions');
 const links = document.querySelectorAll('#menuweb');
 
-toggle.addEventListener('click', () =>
-  weboptions.classList.toggle('active'),
-);
+toggle.addEventListener('click', () => weboptions.classList.toggle('active'),
+  );
 links.forEach(n => n.addEventListener('click', () => {
-  weboptions.classList.remove('active')
-}))
+  weboptions.classList.remove('active');
+}));
