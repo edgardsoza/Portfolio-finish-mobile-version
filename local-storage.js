8d2971bc9d1e8706e89cd15fc641830ec1c4766d
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-02-09 17:33:35 -0600

    fixing linter errors

--- local-storage.js
+++ local-storage.js
@@ -2,7 +2,7 @@ const namefield = document.getElementById('name');
 const emailfield = document.getElementById('email');
 const messagefield = document.getElementById('message');
 const form = document.getElementById('contactform');
-const savedata = localStorage.getItem('savedinformation')
+const savedata = localStorage.getItem('savedinformation');
 
 let object = {
   name: '',
@@ -17,7 +17,7 @@ form.addEventListener('input', () => {
   localStorage.setItem('savedinformation', JSON.stringify(object));
 });
 
-if(savedata) {
+if (savedata) {
   object = JSON.parse(savedata);
   namefield.value = object.name;
   emailfield.value = object.email;
